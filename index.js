a4170027bc4673eac342a3e176d9b1374c072d45
rupom <furqanrupom978@gmail.com>
2023-04-21 02:45:52 +0600

    added res and req form news

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors')
 const categories = require('./categories/categories.json');
+const news = require('./data/news.json')
 const port = process.env.PORT || 5000
 
 app.use(cors());
@@ -11,7 +12,28 @@ app.get('/',(req,res)=>{
 app.get('/categories',(req,res)=>{
     res.send(categories)
 })
+app.get('/news',(req,res)=>{
+    res.send(news)
+})
 
+app.get('/news/:Id',(req,res)=>{
+    const id = req.params.Id
+    // console.log(id)
+    const specificNews = news.find(n => n._id === id);
+    res.send(specificNews);
+})
+
+
+app.get('/categories/:id',(req,res)=>{
+    const id = req.params.id
+    if(id == 0){
+        res.send(news)
+    }
+    else{
+        const categoryNews = news.filter(n=> n.category_id === id)
+        res.send(categoryNews)
+    }
+})
 
 
 
